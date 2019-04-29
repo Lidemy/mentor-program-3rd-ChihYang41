028b94630d6e5c8e6c81b5272abc01750378f4d4
ChihYang41 <Jigsaw0401@gmail.com>
2019-04-29 18:57:43 +0800

    hw1 & hw2 completed

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,9 @@
 function alphaSwap(str) {
-  console.log(str);
+  let output = '';
+  for (let i = 0; i < str.length ; i += 1) {
+    output += (str[i].toUpperCase() !== str[i]) ? str[i].toUpperCase() : str[i].toLowerCase();
+  }
+  return output;
 }
 
 module.exports = alphaSwap;
